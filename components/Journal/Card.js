d5e39b0ee83f3049de5bc811dd2dae9757c5506f
Karthik <karthik97live@gmail.com>
2017-07-04 12:09:52 +0400

    updating local env settings

--- components/Journal/Card.js
+++ components/Journal/Card.js
@@ -12,14 +12,14 @@ class JournalCard extends React.Component {
           href={"/journalDetail?id=" + jModal.customUrlSlug}
           as={"/journal/" + jModal.customUrlSlug}
         >
-          <article className="center ">
+          <article className="center">
             <div className="cf mt1">
-              <span className="fl w-50 db f7 fw6 ttu  black-60 ">
+              <span className="fl w-50 db f7 fw6 ttu black-60 ft-head">
                 <i className="ion-ios-calendar-outline f5 v-mid"></i>&nbsp; {dateVal}
               </span>
             </div>
             <h1 className="fw6 f4 mt1 mb0">{jModal.title}</h1>
-            <p className="lh-copy center f5 black-60 mt2 ft-serif">{jModal.blurb}</p>
+            <p className="lh-copy center f5 black-60 mt2">{jModal.blurb}</p>
             {/*<span className="db f7 fw5 ttu tracked black-50 tl"><i className="f5 v-mid ion-ios-paper-outline"></i>&nbsp;{jModal.type}</span>*/}
           </article>
         </Link>
