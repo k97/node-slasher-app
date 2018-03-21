0095497d57579ddffed4d101fd0b301aae8f589b
karthik <krajendran@fasttrack.com.au>
2018-03-21 16:28:03 +1100

    passphrase page tweaks

--- pages/passphrase.js
+++ pages/passphrase.js
@@ -76,22 +76,27 @@ class Passphrase extends React.Component {
   render() {
     return (
       <Layout title={"Passphrase"}>
-        <div className="body-content">
+        <style global jsx>{`
+          .k-icon-arrow {
+            top: 2px;
+          }
+        `}
+        </style>
+        <div className="mt2 body-content">
 
           <section className="mw5 mw7-ns center bg-white pa3 ph5-ns br2 ba b--black-10">
-
             <button onClick={this.onClickGoBack.bind(this)}
               className="db f6 link dim br2 ba ph3 pv2 mt1 mb3 mr2 dib ba b--black-50 bg-white black-60">
               <i className="ion-ios-arrow-left"></i> &nbsp; Go Back
           </button>
 
-            <h1 className="mt0">Passphrase</h1>
-            <p className="lh-copy measure">Please enter the passphrase to view this part of the website.</p>
+            <h1 className="mt0 mb0 black-60">Private Information</h1>
+            <p className="lh-copy mt2 measure black-50">Please enter the passphrase to proceed to the project.</p>
 
             <form className="db w-100 passphrase-form" onSubmit={(e) => this.onSubmitPass(e)}>
               <input type="text" ref={input => this.passphrase = input} className="mw-100 w-100 w5-ns f5 input-reset ba b--black-20 pv3 ph2 border-box" required />
               <button className="input-reset w-100 w-auto-ns bg-blue f2 ph4" type="submit">
-                <i className="ion-ios-arrow-thin-right white  v-mid"></i>
+                <i className="ion-ios-arrow-thin-right white  v-mid k-icon-arrow"></i>
               </button>
             </form>
 
@@ -107,7 +112,9 @@ class Passphrase extends React.Component {
 
             <div className="lh-copy mt3 mb4 black-60">
               <small className="f6">
-                Some of the projects are locked down in adherence to the respective NDA(s). I intend to share these work only if required for referential purposes. <a className="ka-link dim">Drop me a message here,</a> to recieve the passphrase.
+                Some of the projects are locked down in adherence to the respective NDA(s).
+                I intend to share these work only if required for referential purposes.
+                <a className="ka-link dim"  href="mailto:karthik97live@icloud.com?subject=Access to K97 Portfolio">Drop me a message here,</a> to recieve the passphrase.
             </small>
             </div>
 
